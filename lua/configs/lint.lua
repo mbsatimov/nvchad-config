671a7318ab048f95d96cf81b56b153eb43179069
Mahkambek Satimov <mahkambeksatimov@gmail.com>
2024-09-25 16:48:00 +0500

    init my new neovim config

---
+++ lua/configs/lint.lua
@@ -0,0 +1,13 @@
+require("lint").linters_by_ft = {
+  javascript = { "eslint_d" },
+  typescript = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+}
+
+-- Autocmd for linting while editing
+vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
